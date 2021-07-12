4ad7720c32731cef96d565f539fa37be7e5c92f4
Park_Joyeong <drhamst@gmail.com>
2021-07-13 04:20:24 +0900

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to gameworld
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/gameworld/core_test.clj
@@ -0,0 +1,7 @@
+(ns gameworld.core-test
+  (:require [clojure.test :refer :all]
+            [gameworld.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
