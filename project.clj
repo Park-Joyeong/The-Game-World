fcac9d2543b296a1e669e4606b6e0ddf79ce239a
Park_Joyeong <drhamst@gmail.com>
2021-07-15 13:14:11 +0900

    commit

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
 (defproject gameworld "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Home for gamer"
+  :url "TBD"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [ring "1.8.0"]
+                 [compojure "1.6.2"]]
   :main ^:skip-aot gameworld.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- src/gameworld/core.clj
+++ src/gameworld/core.clj
@@ -1,7 +1,14 @@
 (ns gameworld.core
+  (:require [ring.adapter.jetty :refer [run-jetty]]
+            [compojure.core :refer :all]
+            [compojure.route :as route])
   (:gen-class))
+
+(defroutes app
+  (GET "/" [] "<h1>Hello World!</h1>")
+  (GET "/friends" [] "<h1>There are no friends yet :(</h1>"))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (run-jetty app {:port 80}))
