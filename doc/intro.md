fc423f4652dc78a7bd315a4ad91459567f28ebe2
Park_Joyeong <drhamst@gmail.com>
2021-07-25 23:23:10 +0900

    Delete All Files

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to gameworld
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- test/gameworld/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns gameworld.core-test
-  (:require [clojure.test :refer :all]
-            [gameworld.core :refer :all]))
-
-(deftest a-test
-  (testing "1 is 1"
-    (is (= 1 1))))
